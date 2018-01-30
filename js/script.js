ec667b710cad31534498fe9a1816d6e0e2936347
LeeSD90 <lee.doyle90@gmail.com>
2018-01-30 18:00:37 +0100

    Added game function

--- js/script.js
+++ js/script.js
@@ -2,21 +2,21 @@ function playRound(playerSelection, computerSelection){
 	var playerSelection = playerSelection.toUpperCase();
 	var computerSelection = computerSelection.toUpperCase();
 
-	if(playerSelection === computerSelection){ return "Draw!"; }
+	if(playerSelection === computerSelection){ return false; }
 
 	switch(playerSelection){
 		case 'ROCK':
 			if(computerSelection === "PAPER"){
-				return "You lose! Paper beats Rock";
-			} else return "You win! Rock beats Scissors";
+				return false;
+			} else return true;
 		case 'PAPER':
 			if(computerSelection === "SCISSORS"){
-				return "You lose! Scissors beats Paper";
-			} else return "You win! Paper beats Rock";
+				return false;
+			} else return true;
 		case 'SCISSORS':
 			if(computerSelection === "ROCK" ){
-				return "You lose! Rock beats Scissors";
-			} else return "You win! Scissors beats Paper";
+				return false;
+			} else return true;
 	}
 }
 
@@ -30,4 +30,37 @@ function computerPlay(){
 		default: break;
 	}
 
+}
+
+function game(){
+	var playerScore = 0;
+	var computerScore = 0;
+
+	for(i=0;i<5;i++){
+		var playerSelection = prompt("Type Rock, Paper or Scissors", "Rock");
+		var computerSelection = computerPlay();
+
+		if(playRound(playerSelection, computerSelection)){
+			console.log("You win! " + playerSelection + " beats " + computerSelection);
+			playerScore++;
+		} else{
+			if(computerSelection === playerSelection) { console.log("Draw! You both selected " + computerSelection); }
+			else{
+				console.log("You lose! " + computerSelection + " beats " + playerSelection);
+				computerScore++;
+			}
+		}
+	}
+
+	console.log("Final scores\nPlayer: " + playerScore +"\n" + "Computer " + computerScore);
+
+	if(playerScore == computerScore){ 
+		console.log("It's a draw!");
+	}
+	else if(playerScore > computerScore){
+		console.log("You won!");
+	}
+	else{ 
+		console.log("You lost!"); 
+	}
 }
